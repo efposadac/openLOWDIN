19cf8cd7ae35134030c8a23bf7b5ec08746c0f13
dagonzalezfo <dagonzalez@unal.edu.co>
2015-11-04 13:59:44 -0500

    add new COSMO test

---
+++ test/H2O.APMO.COSMO.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import os
+from colorstring import *
+
+testName = "H2O.APMO.COSMO"
+inputName = testName + ".lowdin"
+outputName = testName + ".out"
+
+# Reference values
+
+refTotalEnergy = -75.956252723159
+
+# Run calculation
+
+os.system ("lowdin2 -i " + inputName)
+
+output = open ( outputName, "r") 
+outputRead = output.readlines()
+
+# Values
+
+for line in outputRead:
+	if "TOTAL ENERGY =" in line :
+		totalEnergy = float(line.split()[3]) 
+
+diffTotalEnergy = abs(refTotalEnergy - totalEnergy)
+
+if (diffTotalEnergy <= 1E-10 ) :
+	print testName + str_green(" ... OK" )
+else :
+	print testName + str_red(" ... NOT OK")
+
+	print "\tDifference HF: " + str( diffTotalEnergy )
+
+output.close()
