11380500943c1a2770addaa19df94ec32f6ea61c
Fernando Posada <efposadac@gmail.com>
2016-10-20 10:52:43 -0500

    Added xmlparser for potentials

---
+++ src/ints/G12Integrals.f90
@@ -0,0 +1,25 @@
+!!  This code is part of LOWDIN Quantum chemistry package                 
+!!  
+!!  this program has been developed under direction of:
+!!
+!!  Prof. A REYES' Lab. Universidad Nacional de Colombia
+!!      http://www.qcc.unal.edu.co
+!!  Prof. R. FLORES' Lab. Universidad de Guadalajara
+!!      http://www.cucei.udg.mx/~robertof
+!!
+!!      Todos los derechos reservados, 2013
+!!
+!!******************************************************************************
+
+!>
+!! @brief Libint2 interface
+!!
+!! @author E. F. Posada
+!!
+!! <b> Creation data : </b> 20-10-2016
+!!
+!! <b> History change: </b>
+!!
+!!   - <tt> 28-04-16 </tt>:  Edwin Posada ( efposadac@unal.edu.co )
+!!        -# Creation of the module.
+
