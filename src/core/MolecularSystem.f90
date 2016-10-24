c64637ee5b028d9a3939337d610f41df05b30a3f
Fernando Posada <efposadac@gmail.com>
2016-10-24 15:30:28 -0500

    * Fixed bug on MolecularSystem * added OMP support for G12 calculation (partially) * Missing inter-species OMP integration.

--- src/core/MolecularSystem.f90
+++ src/core/MolecularSystem.f90
@@ -916,6 +916,7 @@ contains
               read(40,*) i 
               read(40,*) name
               read(40,*) species
+
               call ExternalPotential_load(i, name, species)
 
             end do
