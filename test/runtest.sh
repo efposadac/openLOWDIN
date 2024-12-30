4e3daea681fd5015617a7d86323f115276ed1b7c
jacharrym <jacharrym@unal.edu.co>
2024-12-30 13:37:00 +0100

    Print the timing of each test

--- test/runtest.sh
+++ test/runtest.sh
@@ -10,7 +10,10 @@ echo $lowdinbin
 
 for testfile in `ls *.py`; do
     #echo $testfile
-	python3 $testfile $lowdinbin
+	#python3 $testfile $lowdinbin
+	#/usr/bin/time -v python3 $testfile $lowdinbin 2>&1 | (echo -n $(head -1) ; grep "User time" | gawk '{print "\t" $4 " Sec" }' )
+	/usr/bin/time -o time.log -v python3 $testfile $lowdinbin > out.log  && echo -n $(cat out.log) ; grep "User time" time.log | gawk '{print "\t" $4 " Sec" }'
+
     status=$((status + $?))
 done
 sh clean.sh
