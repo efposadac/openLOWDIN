f40740a7ca4e7553263b617b70bf57d1a6e478d1
jacharrym <jacharrym@unal.edu.co>
2024-12-30 16:27:16 +0100

    Changed user time to walltime in tests script

--- test/runtest.sh
+++ test/runtest.sh
@@ -11,8 +11,9 @@ echo $lowdinbin
 for testfile in `ls *.py`; do
     #echo $testfile
 	#python3 $testfile $lowdinbin
-	#/usr/bin/time -v python3 $testfile $lowdinbin 2>&1 | (echo -n $(head -1) ; grep "User time" | gawk '{print "\t" $4 " Sec" }' )
-	/usr/bin/time -o time.log -v python3 $testfile $lowdinbin > out.log  && echo -n $(cat out.log) ; grep "User time" time.log | gawk '{print "\t" $4 " Sec" }'
+	#/usr/bin/time -o time.log -v python3 $testfile $lowdinbin > out.log  && echo -n $(cat out.log) ; grep "User time" time.log | gawk '{print "\t" $4 " Sec" }'
+	#/usr/bin/time -o time.log -f "%e" python3 $testfile $lowdinbin > out.log  && echo -e -n $(cat out.log) ; echo -e $(cat time.log) " sec" | column -t -s$'\t'
+	/usr/bin/time -o time.log -f "%e" python3 $testfile $lowdinbin > out.log  && printf "%-60s \t %s sec \n" "$(cat out.log)" $(cat time.log) 
 
     status=$((status + $?))
 done
