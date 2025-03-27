93825941f958d5ccefbad17eb696b584d59adaac
Felix Moncada <fsmoncadaa@gmail.com>
2025-03-27 16:49:18 +0100

    Checking if the errors are correctly counted in the github action

--- test/runtest.sh
+++ test/runtest.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-if [ -z $1 ]
-then
+if [ -z $1 ]; then
     lowdinbin="openlowdin"
+    if [ -e ../CONFIG ]; then
+        lowdinbin=`gawk '($1~/EXENAME/){print $3}' ../CONFIG`
+    fi
 else
     lowdinbin=$1
 fi
-echo $lowdinbin
+echo "Testing with lowdinbin:" $lowdinbin
 
 for testfile in `ls *.py`; do
-    #echo $testfile
     python3 $testfile $lowdinbin
     status=$((status + $?))
 done
+
+if [ $status -gt 0 ]; then     
+    echo $status "tests failed"
+else
+    echo "All tests completed successfully"
+fi
 
 exit $status
